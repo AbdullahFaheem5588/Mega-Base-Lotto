7ee5f2b457e1104a679a791e1a9db153393e5ff8
AbdullahFaheem5588 <abdullah.faheem02@gmail.com>
2024-08-26 00:56:06 +0500

    Minor Corrections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Create from "./pages/create/create";
 import LotteryDetails from "./pages/details/LotteryDetails";
